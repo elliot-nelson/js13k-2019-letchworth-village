761a2994b46bbd9287612fee2a48218ddecbb1b4
Elliot Nelson <Elliot.Nelson@hbo.com>
2019-08-20 21:46:29 -0400

    :art: Tinted sprites

--- src/ts/Assets.ts
+++ src/ts/Assets.ts
@@ -1,3 +1,6 @@
+import { Canvas } from "./Canvas";
+import { rgba } from "./Util";
+
 /**
  * Assets
  *
@@ -5,24 +8,41 @@
  * we might need to do, and then saves references to them for later.
  */
 export class Assets {
-  static sword: HTMLImageElement;
-  static player: HTMLImageElement;
-  static demon1: HTMLImageElement;
-  static demon1b: HTMLImageElement;
+  static sword: CanvasImageSource;
+  static player: CanvasImageSource;
+  static demon1: CanvasImageSource;
+  static demon1_hit: CanvasImageSource;
+  static demon1b: CanvasImageSource;
 
   static async init() {
+    // Single PNGs
     this.sword = await this.loadImage('sword.png');
     this.player = await this.loadImage('player_stand_00.png');
     this.demon1 = await this.loadImage('demon1.png');
     this.demon1b = await this.loadImage('demon1b.png');
+
+    // Tinted sprites
+    this.demon1_hit = this.tint(this.demon1, 255, 0, 0, 0.5);
   }
 
-  static async loadImage(uri: string): Promise<HTMLImageElement> {
+  static async loadImage(uri: string): Promise<CanvasImageSource> {
     return new Promise((resolve, reject) => {
       let image = new Image();
       image.onload = () => resolve(image);
       image.onerror = (err) => reject(err);
       image.src = uri;
     });
+  }
+
+  static tint(source: CanvasImageSource, r: number, g: number, b: number, a: number): CanvasImageSource {
+    const canvas = new Canvas(source.width as number, source.height as number);
+    const ctx = canvas.ctx;
+
+    ctx.drawImage(source, 0, 0);
+    ctx.globalCompositeOperation = 'source-in';
+    ctx.fillStyle = rgba(r, g, b, a);
+    ctx.fillRect(0, 0, source.width as number, source.height as number);
+
+    return canvas.canvas;
   }
 }

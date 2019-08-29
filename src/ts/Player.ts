5ac7df954e70d606d99beacff7058881cf6656d7
Elliot Nelson <Elliot.Nelson@hbo.com>
2019-08-29 11:19:24 -0400

    :construction: Move animations out of Player

--- src/ts/Player.ts
+++ src/ts/Player.ts
@@ -1,13 +1,13 @@
 import { game } from './ambient';
 import { Input } from './input';
-import { Assets } from './Assets';
-import { NormalVector, RAD90, RAD45, RAD, Point, Frame } from './Util';
+import { Assets, Behavior, Animation2, Sprite } from './Assets';
+import { Point, NormalVector, RAD, rotatePolygon, Polygon } from './Geometry';
+import { Frame } from './Assets';
 
 /**
  * Player
  */
 export class Player {
-  state: Player.State;
   x: number;
   y: number;
   next: Point;
@@ -18,7 +18,6 @@ export class Player {
   frameQ: Frame[];
 
   constructor() {
-    this.state = Player.State.NEUTRAL;
     this.x = 60;
     this.y = 60;
     this.facing = { x: 0, y: -1, m: 0 };
@@ -26,107 +25,70 @@ export class Player {
     this.frameQ = [];
   }
 
-  update() {
-    // Grab the next scheduled frame for our player, or if one
-    // doesn't exist, use a default.
-    this.frame = this.frameQ.shift() || { input: true };
+  startAnimation(animation: Animation2) {
+    this.frameQ = animation.frames.slice();
+    this.frame = this.frameQ.shift();
+  }
 
-    if (this.frame.input) {
+  nextAnimationFrame(defaultAnimation?: Animation2) {
+    if (this.frameQ.length === 0) {
+      this.startAnimation(defaultAnimation);
+    } else {
+      this.frame = this.frameQ.shift();
+    }
+  }
+
+  update() {
+    this.nextAnimationFrame(Animation2.player_walk);
+
+    // Do a "first pass" behavior check, so that we can respond on *this frame*
+    // to input presses. You could simplify a bit by doing a single pass, with
+    // the downside that the 1st frame of the attack animation would come out on the
+    // frame following the input instead of the same frame.
+    if (this.frame.behavior === Behavior.DEFAULT) {
       if (game.input.direction.m > 0) {
         this.facing = game.input.direction;
         this.facingAngle = Math.atan2(this.facing.y, this.facing.x);
       }
 
       if (game.input.pressed[Input.Action.DODGE]) {
-        this.frameQ.push({ move: { ...this.facing, m: 2 }, invuln: false });
-        this.frameQ.push({ move: { ...this.facing, m: 4 }, invuln: true });
-        this.frameQ.push({ move: { ...this.facing, m: 6 }, invuln: true });
-        this.frameQ.push({ move: { ...this.facing, m: 7 }, invuln: true });
-        this.frameQ.push({ move: { ...this.facing, m: 7 }, invuln: true });
-        this.frameQ.push({ move: { ...this.facing, m: 2 }, invuln: false });  // 6*4 = 24, ~28
-
-        // For debugging
-        /*this.frameQ.push({ move: { ...this.facing, m: 5 }, invuln: true });
-        this.frameQ.push({ move: { ...this.facing, m: 5 }, invuln: true });
-        this.frameQ.push({ move: { ...this.facing, m: 5 }, invuln: true });
-        this.frameQ.push({ move: { ...this.facing, m: 5 }, invuln: true });
-        this.frameQ.push({ move: { ...this.facing, m: 5 }, invuln: true });
-        this.frameQ.push({ move: { ...this.facing, m: 5 }, invuln: true });
-        this.frameQ.push({ move: { ...this.facing, m: 5 }, invuln: true });*/
-        // End debugging
-
-        this.frame = this.frameQ.shift();
+        this.startAnimation(Animation2.player_dodge);
       } else if (game.input.pressed[Input.Action.ATTACK]) {
-        let hitbox = { r: 64, a1: this.facingAngle - RAD[55], a2: this.facingAngle + RAD[55] };
-        this.frameQ.push({ move: { ...this.facing, m: 4 } });
-        this.frameQ.push({ move: { ...this.facing, m: 2 }, hitbox });
-        this.frameQ.push({ move: { ...this.facing, m: 0 }, hitbox });
-        this.frameQ.push({ move: { ...this.facing, m: 0 }, hitbox });
-        this.frameQ.push({ move: { ...this.facing, m: 0 }, hitbox });
-        this.frameQ.push({ move: { ...this.facing, m: 0 } });
-
-        this.frame = this.frameQ.shift();
-      } else {
-        this.frame.move = game.input.direction.m > 0 ? this.facing : undefined;
+        this.startAnimation(Animation2.player_attack);
       }
     }
 
-    if (this.frame.move) {
-      this.next = {
-        x: this.x + this.frame.move.x * this.frame.move.m * 4,
-        y: this.y + this.frame.move.y * this.frame.move.m * 4
-      };
-    } else {
-      this.next = { x: this.x, y: this.y };
-    }
+    // If only we had "this.frame.m ?? blah" :)
+    let motion = this.frame.m === undefined ? (game.input.direction.m * 4) : this.frame.m;
+
+    this.next = {
+      x: this.x + this.facing.x * motion,
+      y: this.y + this.facing.y * motion
+    };
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    ctx.fillStyle = 'rgba(255, 255, 255, 255)';
-
-    // draw bbox
-    ctx.strokeStyle = 'rgba(255, 255, 0, 0.7)';
-    ctx.strokeRect(this.x - this.bbox(), this.y - this.bbox(), this.bbox() * 2, this.bbox() * 2);
-
     ctx.imageSmoothingEnabled = false;
     ctx.save();
     ctx.translate(this.x, this.y);
-    ctx.rotate(this.facingAngle + RAD90);
+    ctx.rotate(this.facingAngle + RAD[90]);
     if (this.frame.invuln) {
       ctx.globalAlpha = 0.5;
     }
-    ctx.drawImage(Assets.player, 0, 0, 32, 32, -16, -16, 32, 32);
-    /*ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, -50);
-    ctx.stroke();*/
-
-    ctx.translate(30, 0);
-    ctx.rotate(RAD45 + RAD90);
-    ctx.drawImage(Assets.sword, 0, 0, 32, 32, -16, -16, 32, 32);
-    ctx.restore();
-
-    // Hitbox
-    if (this.frame.hitbox) {
-      ctx.beginPath();
-      ctx.fillStyle = 'rgba(255, 0, 0, 0.75)';
-      ctx.arc(this.x, this.y, this.frame.hitbox.r, this.frame.hitbox.a1, this.frame.hitbox.a2, false);
-      ctx.lineTo(this.x, this.y);
-      ctx.fill();
-      //console.log(this.frame.hitbox);
-    }
-
+    Sprite.drawSprite(ctx, this.frame.sprite, 0, 0);
+    Sprite.drawBoundingBox(ctx, this.frame.sprite, 0, 0);
+    Sprite.drawHitBox(ctx, this.frame.sprite, 0, 0);
     ctx.globalAlpha = 1;
+    ctx.restore();
   }
 
-  bbox(): number {
-    return 10;
+  getBoundingPolygon(): Polygon {
+    return rotatePolygon(Sprite.getBoundingBoxPolygon(this.frame.sprite, this.x, this.y), this.facingAngle);
   }
-}
 
-export namespace Player {
-  export const enum State {
-    NEUTRAL = 1,
-    DASHING = 2
+  getHitboxPolygon(): Polygon|undefined {
+    if (this.frame.sprite.hbox) {
+      return rotatePolygon(Sprite.getBoundingBoxPolygon(this.frame.sprite, this.x, this.y), this.facingAngle);
+    }
   }
 }

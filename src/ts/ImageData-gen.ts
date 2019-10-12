1baa18714b305e9360a1662feefbe56dc94bdb55
Elliot Nelson <Elliot.Nelson@hbo.com>
2019-10-12 16:21:24 -0400

    :bento: Check in output of sprite generator

---
+++ src/ts/ImageData-gen.ts
@@ -0,0 +1,41 @@
+/* Image data */
+
+export const ImageData =
+/* <generated> */
+{ demon1_1: { x: 0, y: 0, width: 16, height: 22 },
+  demon1_2: { x: 16, y: 0, width: 16, height: 22 },
+  demon1_3: { x: 32, y: 0, width: 16, height: 22 },
+  demon1_4: { x: 48, y: 0, width: 16, height: 22 },
+  demon1_5: { x: 64, y: 0, width: 16, height: 22 },
+  demon1_6: { x: 80, y: 0, width: 16, height: 22 },
+  demon1_7: { x: 96, y: 0, width: 16, height: 22 },
+  demon1_8: { x: 112, y: 0, width: 16, height: 22 },
+  demon1_9: { x: 128, y: 0, width: 16, height: 22 },
+  demon1_10: { x: 144, y: 0, width: 16, height: 22 },
+  particles_1: { x: 0, y: 22, width: 11, height: 11 },
+  player_1: { x: 0, y: 33, width: 64, height: 64 },
+  player_2: { x: 64, y: 33, width: 64, height: 64 },
+  player_3: { x: 128, y: 33, width: 64, height: 64 },
+  player_4: { x: 192, y: 33, width: 64, height: 64 },
+  player_5: { x: 256, y: 33, width: 64, height: 64 },
+  player_6: { x: 320, y: 33, width: 64, height: 64 },
+  player_7: { x: 384, y: 33, width: 64, height: 64 },
+  player_8: { x: 448, y: 33, width: 64, height: 64 },
+  player_9: { x: 512, y: 33, width: 64, height: 64 },
+  portal_1: { x: 0, y: 97, width: 32, height: 32 },
+  swordmeter_1: { x: 0, y: 129, width: 48, height: 128 },
+  swordmeter_2: { x: 48, y: 129, width: 48, height: 128 },
+  swordmeter_3: { x: 96, y: 129, width: 48, height: 128 },
+  swordmeter_4: { x: 144, y: 129, width: 48, height: 128 },
+  swordmeter_5: { x: 192, y: 129, width: 48, height: 128 },
+  tiles_1: { x: 0, y: 257, width: 32, height: 32 },
+  tiles_2: { x: 32, y: 257, width: 32, height: 32 },
+  tiles_3: { x: 64, y: 257, width: 32, height: 32 },
+  tiles_4: { x: 96, y: 257, width: 32, height: 32 },
+  tiles_5: { x: 128, y: 257, width: 32, height: 32 },
+  tiles_6: { x: 160, y: 257, width: 32, height: 32 },
+  tiles_7: { x: 192, y: 257, width: 32, height: 32 },
+  tiles_8: { x: 224, y: 257, width: 32, height: 32 },
+  tiles_9: { x: 256, y: 257, width: 32, height: 32 } }
+/* </generated> */
+;
